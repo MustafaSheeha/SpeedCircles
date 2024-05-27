67e2b1b65a314132da24cec35fa0dd78040a1fe2
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 07:55:07 +0300

    Add logo and slogan widgets to SplashView

--- lib/features/splash/view/splash_view.dart
+++ lib/features/splash/view/splash_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:speedcircles/core/constants/app_assets.dart';
+import 'package:speedcircles/core/constants/app_strings.dart';
+
+import '../../../core/widgets/logo_widget.dart';
+import '../../../core/widgets/slogan_widget.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          LogoWidget(),
+          SloganWidget(),
+        ],
+      ),
+    );
   }
 }
