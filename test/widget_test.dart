375fb9d66983bc27baaa08787dfdf00a95f4838c
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 07:56:01 +0300

    Fix  MyApp widget import to widget test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:speedcircles/my_app.dart';
 
-import 'package:speedcircles/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
