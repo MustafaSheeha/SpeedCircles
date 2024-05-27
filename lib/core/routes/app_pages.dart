df486dcb70dc30b4ce64584311032f38bbbd7315
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 04:51:27 +0300

    Add initial app pages and splash screen route to app routes.

---
+++ lib/core/routes/app_pages.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:speedcircles/core/routes/app_routes.dart';
+import 'package:speedcircles/features/splash/bindings/splash_binding.dart';
+
+import '../../features/splash/view/splash_view.dart';
+
+
+Transition defaultTransition = Transition.zoom;
+Duration defaultTransitionDuration = const Duration(milliseconds: 300);
+
+abstract class AppPages {
+  static const initial = AppRouteNames.splash;
+
+  static final routesList = [
+    GetPage(
+      name: AppRouteNames.splash,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+      transition: defaultTransition,
+      transitionDuration: defaultTransitionDuration,
+    ),
+  ];
+}
