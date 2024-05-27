4575f61425fbd9f53f0b438b62a3f0f801d7fb38
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 03:22:08 +0300

    Project Start Point

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:speedcircles/my_app.dart';
+import 'package:speedcircles/splash_view.dart';
+
+void main() {
+  
+  runApp(const MyApp());
+}
