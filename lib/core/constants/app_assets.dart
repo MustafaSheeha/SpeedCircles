36c21a577df170354201eb6e3dd7d4b8743302af
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 07:39:16 +0300

    Create AppAssets and AppStrings

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,4 @@
+abstract class AppAssets {
+  static const String logoNoBackground = "assets/logo-no-background.png";
+}
+
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,3 @@
+abstract class AppStrings {
+  static const slogan = "Your Ultimate Tire Destination";
+}
