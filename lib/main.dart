0aeed7329b6bc7d02aca120df8034a2081126b69
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-27 04:52:49 +0300

    Refactor app initialization and routing
    Removed unnecessary imports and updated the app initialization and routing in MyApp to use the AppPages and AppRoutes classes. Also set debugShowCheckedModeBanner to false and set the initial route to the splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
 import 'package:speedcircles/my_app.dart';
-import 'package:speedcircles/splash_view.dart';
 
 void main() {
   
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:speedcircles/core/routes/app_pages.dart';
+import 'package:speedcircles/core/routes/app_routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp();
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,initialRoute: AppRouteNames.splash,
+      getPages: AppPages.routesList,
+    );
   }
 }
