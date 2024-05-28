5d6b01dba72dd93409a4422fdb2d3d40416be20d
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-28 21:29:14 +0300

    Add image assets to AppAssets class.

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,20 @@
-abstract class AppAssets {
+class AppAssets {
+  AppAssets._();
+  
+  /// Assets for imagesLogoNoBackground
+  /// assets/images/logo-no-background.png
   static const String logoNoBackground = "assets/images/logo-no-background.png";
+
+  /// Assets for imagesVarietyAndReasonablePrice
+  /// assets/images/varietyAndReasonablePrice.png
+  static const String varietyAndReasonablePrice = "assets/images/varietyAndReasonablePrice.png";
+
+  /// Assets for imagesVariousPayment
+  /// assets/images/variousPayment.png
+  static const String variousPayment = "assets/images/variousPayment.png";
+
+  /// Assets for imagesWarranty
+  /// assets/images/warranty.jpg
+  static const String warranty = "assets/images/warranty.jpg";
 }
+
