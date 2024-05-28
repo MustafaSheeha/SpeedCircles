f8b75e5311209193abd8868308cf0571459f0102
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-28 20:42:03 +0300

    Update logoNoBackground asset path in AppAssets class

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,3 @@
 abstract class AppAssets {
-  static const String logoNoBackground = "assets/logo-no-background.png";
+  static const String logoNoBackground = "assets/images/logo-no-background.png";
 }
