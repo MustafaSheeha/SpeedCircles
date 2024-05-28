a23d1574dec650e329a24cd4fda97b9dd73ccc91
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-28 23:11:53 +0300

    Add new text style and update app strings.

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -3,11 +3,16 @@ abstract class AppStrings {
   static const gabrielaFont = "Gabriela";
   static const hitchHikeFont = "hitch-hike";
   static const skip = "skip";
+  static const next = "Next";
   static const varietyAndReasonablePrice = "Variety and Reasonable Price";
-  static const varietyAndReasonablePriceDescription = "We offer a variety of international tires. With high quality and the right price, whatever your tire size and budget, we are able to meet your request.";
+  static const varietyAndReasonablePriceDescription =
+      "We offer a variety of international tires. With high quality and the right price, whatever your tire size and budget, we are able to meet your request.";
   static const warranty = "Warranty";
-  static const warrantyDescription = "All tires available on our website are brand new, full value and are subject to the manufacturer's warranty against manufacturing defects.";
-  static const variousPaymentMethodsAndInstallments = "Various Payment Methods and Installments";
-  static const variousPaymentMethodsAndInstallmentsDescription = "Possibility of payment in cash or through multiple bank cards. With the possibility of installments through multiple options.";
-
+  static const warrantyDescription =
+      "All tires available on our website are brand new, full value and are subject to the manufacturer's warranty against manufacturing defects.";
+  static const variousPaymentMethodsAndInstallments =
+      "Various Payment Methods and Installments";
+  static const variousPaymentMethodsAndInstallmentsDescription =
+      "Possibility of payment in cash or through multiple bank cards. With the possibility of installments through multiple options.";
+  static const startNow = 'Start Now';
 }
