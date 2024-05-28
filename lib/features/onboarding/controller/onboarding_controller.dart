6cdfaf21966003618866d969196fc1108959f94f
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-28 22:10:52 +0300

    Add page controller and update page index in OnboardingController

--- lib/features/onboarding/controller/onboarding_controller.dart
+++ lib/features/onboarding/controller/onboarding_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:speedcircles/core/constants/app_assets.dart';
 import 'package:speedcircles/core/constants/app_strings.dart';
@@ -5,23 +6,31 @@ import 'package:speedcircles/core/constants/app_strings.dart';
 import '../model/onboarding_model.dart';
 
 class OnboardingController extends GetxController {
+  int pageCurrentIndex = 0;
+  PageController? pageController;
   List<OnboardingModel> onboardingModelList = [];
+  updatepageCurrentIndex(int value) {
+    pageCurrentIndex = value;
+    update();
+  }
 
   @override
   void onInit() {
+    pageController = PageController();
     onboardingModelList = [
       OnboardingModel(
           description: AppStrings.varietyAndReasonablePriceDescription,
           image: AppAssets.varietyAndReasonablePrice,
           title: AppStrings.varietyAndReasonablePrice),
       OnboardingModel(
-          description: AppStrings.variousPaymentMethodsAndInstallmentsDescription,
+          description:
+              AppStrings.variousPaymentMethodsAndInstallmentsDescription,
           image: AppAssets.variousPayment,
           title: AppStrings.variousPaymentMethodsAndInstallments),
       OnboardingModel(
           description: AppStrings.warrantyDescription,
           image: AppAssets.warranty,
-          title: AppStrings.warrantyDescription),
+          title: AppStrings.warranty),
     ];
     super.onInit();
   }
