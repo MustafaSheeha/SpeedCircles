8e807a0aa7d20307e11807f06aa412c3a96c896b
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-28 20:42:11 +0300

    Add new colors to AppColors class

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const primary = Color(0xff00ab04);
+  static const grey = Color(0xff4c4c4c);
+  static const white = Colors.white;
+  static const black = Colors.black;
   static const scaffoldBackground = Color(0xfff0f4ea);
 }
