206b3891ad636fb68e332179d448840b18f45ed6
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-05-29 00:47:54 +0300

    Add functions to set preferred orientations and system UI overlay style.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:get/get.dart';
 import 'package:speedcircles/core/database/cache/cache_helper.dart';
 import 'package:speedcircles/my_app.dart';
 
+import 'core/functions/set_preferred_orientations.dart';
+import 'core/functions/set_system_ui_overlay_style.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPreferredOrientations();
+  setSystemUIOverlayStyle();
   CacheHelper().init();
   Get.put(CacheHelper());
   runApp(const MyApp());
