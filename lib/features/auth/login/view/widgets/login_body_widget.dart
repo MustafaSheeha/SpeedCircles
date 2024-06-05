99ced0c5fe27b7bed07b12d2c18225513db015b6
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 09:11:09 +0300

    Refactor LoginBodyWidget
    Removed unused imports and unnecessary vertical space widget.

--- lib/features/auth/login/view/widgets/login_body_widget.dart
+++ lib/features/auth/login/view/widgets/login_body_widget.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:speedcircles/core/constants/app_colors.dart';
 import 'package:speedcircles/features/auth/login/view/widgets/login_form_widget.dart';
 import 'package:speedcircles/features/auth/login/view/widgets/welcome_login_widget.dart';
-import '../../../../../core/widgets/vertical_space.dart';
 import '../../../widgets/auth_container_widget.dart';
 
 class LoginBodyWidget extends StatelessWidget {
