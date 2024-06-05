0f0afd616dea1454e9ae41866bbfde67ed37471e
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 05:35:23 +0300

    Add background clipper to login view
    Added a new widget `BackgroundClipper` to the login view to create a visually appealing background. The widget is used twice in the login view to create a symmetrical design.

--- lib/features/auth/login/view/login_view.dart
+++ lib/features/auth/login/view/login_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:speedcircles/core/constants/app_colors.dart';
+
+import 'widgets/background_clipper.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          BackgroundClipper(),
+          Spacer(),
+          BackgroundClipper(
+            quarterTurns: 2,
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/auth/login/view/widgets/background_clipper.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../../core/constants/app_colors.dart';
+
+class BackgroundClipper extends StatelessWidget {
+  const BackgroundClipper({
+    super.key,
+    this.quarterTurns = 0,
+  });
+  final int? quarterTurns;
+  @override
+  Widget build(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: quarterTurns!,
+      child: ClipPath(
+        clipper: ContainerClipper(),
+        child: Container(
+          height: Get.height * 0.15,
+          color: AppColors.primary,
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+      size.width / 4,
+      size.height * 0.1,
+      size.width,
+      0,
+    );
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
