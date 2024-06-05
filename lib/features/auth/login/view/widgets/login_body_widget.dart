aa576894b703e40dc82bac0acc0d89e5217b0e29
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 09:10:34 +0300

    Refactor login body widget to use auth container widget
    The login body widget was refactored to use the newly created auth container widget. This widget contains the login form and welcome login widgets and provides a consistent styling for the login page.

--- lib/features/auth/login/view/widgets/login_body_widget.dart
+++ lib/features/auth/login/view/widgets/login_body_widget.dart
@@ -4,37 +4,16 @@ import 'package:speedcircles/core/constants/app_colors.dart';
 import 'package:speedcircles/features/auth/login/view/widgets/login_form_widget.dart';
 import 'package:speedcircles/features/auth/login/view/widgets/welcome_login_widget.dart';
 import '../../../../../core/widgets/vertical_space.dart';
+import '../../../widgets/auth_container_widget.dart';
 
 class LoginBodyWidget extends StatelessWidget {
   const LoginBodyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
-      padding: EdgeInsets.symmetric(horizontal: Get.width * 0.05),
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.primary),
-        color: AppColors.white.withOpacity(0.9),
-        borderRadius: BorderRadius.circular(22),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.grey.withOpacity(0.3),
-            blurRadius: 2,
-            spreadRadius: 3,
-            offset: const Offset(2, 5),
-          )
-        ],
-      ),
-      child: Column(
-        children: [
-          VerticalSpace(Get.height * 0.04),
-          const WelcomeLoginWidget(),
-          VerticalSpace(Get.height * 0.04),
-          const LoginFormWidget(),
-          VerticalSpace(Get.height * 0.015),
-        ],
-      ),
+    return const AuthContainerWidget(
+      welcomeLoginWidget: WelcomeLoginWidget(),
+      formWidget: LoginFormWidget(),
     );
   }
 }
---
+++ lib/features/auth/widgets/auth_container_widget.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/constants/app_colors.dart';
+import '../../../core/widgets/vertical_space.dart';
+
+class AuthContainerWidget extends StatelessWidget {
+  const AuthContainerWidget({
+    super.key, required this.welcomeLoginWidget, required this.formWidget,
+  });
+  final Widget welcomeLoginWidget;
+  final Widget formWidget;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
+      padding: EdgeInsets.symmetric(horizontal: Get.width * 0.05),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.primary),
+        color: AppColors.white.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(22),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.grey.withOpacity(0.3),
+            blurRadius: 2,
+            spreadRadius: 3,
+            offset: const Offset(2, 5),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          VerticalSpace(Get.height * 0.04),
+          welcomeLoginWidget,
+          VerticalSpace(Get.height * 0.04),
+          formWidget,
+          VerticalSpace(Get.height * 0.015),
+        ],
+      ),
+    );
+  }
+}
