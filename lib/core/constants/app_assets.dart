4e85929d224222834aca1be27c0b02fc724a2ba5
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 10:58:30 +0300

    Add new assets and update AppAssets constants
    Added new images for Facebook, Google, and Twitter and updated the AppAssets constants to include them. Also added new font assets and updated the naming convention for some existing image assets.

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,19 +1,42 @@
-abstract class AppAssets {
-  
+class AppAssets {
+  AppAssets._();
+
+  /// Assets for fontsGabrielaRegular
+  /// assets/fonts/Gabriela-Regular.ttf
+  static const String fontsGabrielaRegular =
+      "assets/fonts/Gabriela-Regular.ttf";
+
+  /// Assets for fontsHitchHike
+  /// assets/fonts/hitch-hike.ttf
+  static const String fontsHitchHike = "assets/fonts/hitch-hike.ttf";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.png
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.png
+  static const String imagesGoogle = "assets/images/google.png";
+
   /// Assets for imagesLogoNoBackground
   /// assets/images/logo-no-background.png
-  static const String logoNoBackground = "assets/images/logo-no-background.png";
+  static const String imagesLogoNoBackground =
+      "assets/images/logo-no-background.png";
 
-  /// Assets for imagesVarietyAndReasonablePrice
-  /// assets/images/varietyAndReasonablePrice.png
-  static const String varietyAndReasonablePrice = "assets/images/varietyReasonablePrice.png";
+  /// Assets for imagesTwitter
+  /// assets/images/twitter.png
+  static const String imagesTwitter = "assets/images/twitter.png";
+
+  /// Assets for imagesVarietyReasonablePrice
+  /// assets/images/varietyReasonablePrice.png
+  static const String imagesVarietyReasonablePrice =
+      "assets/images/varietyReasonablePrice.png";
 
   /// Assets for imagesVariousPayment
   /// assets/images/variousPayment.png
-  static const String variousPayment = "assets/images/variousPayment.png";
+  static const String imagesVariousPayment = "assets/images/variousPayment.png";
 
   /// Assets for imagesWarranty
   /// assets/images/warranty.jpg
-  static const String warranty = "assets/images/warranty.jpg";
+  static const String imagesWarranty = "assets/images/warranty.jpg";
 }
-
