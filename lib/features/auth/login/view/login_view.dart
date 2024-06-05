5666044f9fc98ce4fae87f45ea97ca793f339c1b
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 08:54:50 +0300

    Refactor login view to use AuthTemplateWidget
    The commit refactors the login view to use the `AuthTemplateWidget` instead of manually creating a `SingleChildScrollView` and `Column` widgets. This improves code reusability and reduces code duplication. The `LoginBodyWidget` and `NewAccountWidget` are now passed as children to the `AuthTemplateWidget`.

--- lib/features/auth/login/view/login_view.dart
+++ lib/features/auth/login/view/login_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:speedcircles/core/routes/app_route_names.dart';
-import 'package:speedcircles/core/widgets/logo_widget.dart';
+import 'package:speedcircles/features/auth/auth_template_widget.dart';
 import 'package:speedcircles/features/auth/login/view/widgets/new_account_widget.dart';
 import '../../../../core/widgets/vertical_space.dart';
-import 'widgets/background_clipper.dart';
 import 'widgets/login_body_widget.dart';
 
 class LoginView extends StatelessWidget {
@@ -13,29 +11,13 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        child: SizedBox(
-          height: Get.height,
-          child: Column(
-            children: [
-              const BackgroundClipper(),
-              const LogoWidget(),
-              VerticalSpace(Get.height * 0.04),
-              const LoginBodyWidget(),
-              VerticalSpace(Get.height * 0.04),
-               NewAccountWidget(
-                onPressed: ()=>Get.offNamed(AppRouteNames.register),
-              ),
-              const Spacer(),
-              const BackgroundClipper(
-                quarterTurns: 2,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        body: AuthTemplateWidget(
+            authTemplateBody: Column(
+      children: [
+        const LoginBodyWidget(),
+        VerticalSpace(Get.height * 0.02),
+        const NewAccountWidget(),
+      ],
+    )));
   }
 }
