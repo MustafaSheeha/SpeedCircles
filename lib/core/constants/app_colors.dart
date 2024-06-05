7866b3feb01c85bef8e6b2943ba16ea92b3a7691
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 10:58:46 +0300

    Add light grey color constant to AppColors class.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const primary = Color(0xff00ab04);
   static const grey = Color(0xff4c4c4c);
+  static const lightGrey = Color(0xffdcdcdc);
   static const white = Colors.white;
   static const black = Colors.black;
   static const scaffoldBackground = Color(0xfff0f4ea);
