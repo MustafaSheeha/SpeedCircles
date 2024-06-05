49fdb3d7bdf13642b8619685a4361b2a6105bcd7
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 06:31:11 +0300

    Refactor login view and add login body widget.

--- lib/features/auth/login/view/login_view.dart
+++ lib/features/auth/login/view/login_view.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:speedcircles/core/constants/app_colors.dart';
 import 'package:speedcircles/core/widgets/logo_widget.dart';
-
+import '../../../../core/widgets/vertical_space.dart';
 import 'widgets/background_clipper.dart';
+import 'widgets/login_body_widget.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Column(
         children: [
-          BackgroundClipper(),
-          LogoWidget(),
-          Spacer(),
-          BackgroundClipper(
+          const BackgroundClipper(),
+          const LogoWidget(),
+          VerticalSpace(Get.height * 0.05),
+          const LoginBodyWidget(),
+          const Spacer(),
+          const BackgroundClipper(
             quarterTurns: 2,
           ),
         ],
---
+++ lib/features/auth/login/view/widgets/login_body_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:speedcircles/core/constants/app_colors.dart';
+import 'package:speedcircles/features/auth/login/view/widgets/login_form_widget.dart';
+import 'package:speedcircles/features/auth/login/view/widgets/welcome_login_widget.dart';
+import '../../../../../core/widgets/vertical_space.dart';
+
+class LoginBodyWidget extends StatelessWidget {
+  const LoginBodyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
+      padding: EdgeInsets.symmetric(horizontal: Get.width * 0.1),
+      height: Get.height * 0.5,
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.primary),
+        color: AppColors.white.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(22),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.grey.withOpacity(0.3),
+            blurRadius: 2,
+            spreadRadius: 3,
+            offset: const Offset(2, 5),
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          VerticalSpace(Get.height * 0.05),
+          const WelcomeLoginWidget(),
+          VerticalSpace(Get.height * 0.01),
+          const LoginFormWidget()
+        ],
+      ),
+    );
+  }
+}
