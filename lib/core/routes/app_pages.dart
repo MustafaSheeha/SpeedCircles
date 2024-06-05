6603867ad34a3c5f2a6060fa4cfcaecbcb7122e7
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-05 07:52:02 +0300

    Add login and register routes.

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
-import 'package:speedcircles/core/routes/app_routes.dart';
+import 'package:speedcircles/core/routes/app_route_names.dart';
+import 'package:speedcircles/features/auth/login/view/login_view.dart';
+import 'package:speedcircles/features/auth/register/view/register_view.dart';
 import 'package:speedcircles/features/onboarding/bindings/onboarding_binding.dart';
 import 'package:speedcircles/features/onboarding/view/onboarding_view.dart';
 import 'package:speedcircles/features/splash/bindings/splash_binding.dart';
@@ -24,6 +26,18 @@ abstract class AppPages {
       name: AppRouteNames.onboarding,
       page: () => const OnboardingView(),
       binding: OnboardingBinding(),
+      transition: defaultTransition,
+      transitionDuration: defaultTransitionDuration,
+    ),
+    GetPage(
+      name: AppRouteNames.login,
+      page: ()=> const LoginView(),
+      transition: defaultTransition,
+      transitionDuration: defaultTransitionDuration,
+    ),
+    GetPage(
+      name: AppRouteNames.register,
+      page: ()=> const RegisterView(),
       transition: defaultTransition,
       transitionDuration: defaultTransitionDuration,
     ),
