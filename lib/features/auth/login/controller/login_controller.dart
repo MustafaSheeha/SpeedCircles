88431d3b4ea021094ff913855ae896001ea450af
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 15:17:39 +0300

    Add login functionality to RegisterController.

--- lib/features/auth/login/controller/login_controller.dart
+++ lib/features/auth/login/controller/login_controller.dart
@@ -8,32 +8,28 @@ import 'package:speedcircles/features/auth/model/user_model.dart';
 import 'package:speedcircles/features/auth/services/auth_service.dart';
 
 class RegisterController extends GetxController {
-  final fullNameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  final confirmPasswordController = TextEditingController();
   final _authService = AuthService.to;
 
   final UserModel userModel = UserModel();
-  register() async {
-    try {
-      userModel.fullName = fullNameController.text;
+
+login()async{
+   try {
       userModel.email = emailController.text;
       userModel.password = passwordController.text;
-      userModel.confirmPassword = confirmPasswordController.text;
-      await _authService.createUserWithEmailAndPassword(
+      await _authService.signInWithEmailAndPassword(
           email: userModel.email!, password: userModel.password!);
       Get.snackbar(
-          AppStrings.success, AppStrings.yourAccountWasSuccessfullyCreated);
-      log(AppStrings.yourAccountWasSuccessfullyCreated);
+          AppStrings.success, AppStrings.successfulLogin);
+      log(AppStrings.successfulLogin);
     } on FirebaseAuthException catch (e) {
       _handleFirebaseAuthException(e);
     } catch (e) {
       Get.snackbar(AppStrings.authenticationError, e.toString());
       log(e.toString());
     }
-  }
-
+}
   void _handleFirebaseAuthException(FirebaseAuthException e) {
     String errorMessage;
     switch (e.code) {
