177b8cd843c64136e86b2d1dcefe38900a49d347
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 14:54:09 +0300

    Refactor createUserWithEmailAndPassword method in AuthService
    Simplify the createUserWithEmailAndPassword method in AuthService by removing unnecessary try-catch blocks and handling of specific exceptions. The method now returns the user object directly from the UserCredential object.

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -7,19 +7,8 @@ class AuthService extends GetxService {
 
   Future<User?> createUserWithEmailAndPassword(
       {required String email, required String password}) async {
-    try {
-      UserCredential userCredential = await _auth
-          .createUserWithEmailAndPassword(email: email, password: password);
-      return userCredential.user;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
-    }
-    return null;
+    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return userCredential.user;
   }
 }
