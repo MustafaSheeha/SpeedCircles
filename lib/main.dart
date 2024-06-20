11ca4f07b94465860c8e6f6d487a0b679e1a92d8
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 11:52:13 +0300

    Connecting to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:speedcircles/core/database/cache/cache_helper.dart';
+import 'package:speedcircles/firebase_options.dart';
 import 'package:speedcircles/my_app.dart';
 
 import 'core/functions/set_preferred_orientations.dart';
@@ -8,6 +10,9 @@ import 'core/functions/set_system_ui_overlay_style.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setPreferredOrientations();
   setSystemUIOverlayStyle();
   CacheHelper().init();
