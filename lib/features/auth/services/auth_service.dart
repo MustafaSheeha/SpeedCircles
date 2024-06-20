258db5767faef88c4c6809d7c65ce5fc060e0161
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 15:11:20 +0300

    AuthService Add a new method for signing in with email and password.

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -5,10 +5,25 @@ class AuthService extends GetxService {
   static AuthService get to => Get.find();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> createUserWithEmailAndPassword(
-      {required String email, required String password}) async {
+  Future<User?> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: email, password: password);
+      email: email,
+      password: password,
+    );
+    return userCredential.user;
+  }
+
+  Future<User?> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
     return userCredential.user;
   }
 }
