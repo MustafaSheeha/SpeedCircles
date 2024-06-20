001fbc08f037b9fa8313c95a022e1de4ed6122e3
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 14:54:29 +0300

    Add error handling to RegisterController's register method
    This commit adds error handling to the register method in RegisterController
    to handle Firebase authentication exceptions and other errors that may occur
    during user registration. The error messages are displayed to the user using
    Get.snackbar and also logged using the dart:developer library.

--- lib/features/auth/register/controller/register_controller.dart
+++ lib/features/auth/register/controller/register_controller.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:speedcircles/core/constants/app_strings.dart';
 import 'package:speedcircles/features/auth/model/user_model.dart';
 import 'package:speedcircles/features/auth/services/auth_service.dart';
 
@@ -11,11 +15,59 @@ class RegisterController extends GetxController {
 
   final UserModel userModel = UserModel();
   register() async {
-    userModel.fullName = fullNameController.text;
-    userModel.email = emailController.text;
-    userModel.password = passwordController.text;
-    userModel.confirmPassword = confirmPasswordController.text;
-    await AuthService.to.createUserWithEmailAndPassword(
-        email: userModel.email!, password: userModel.password!);
+    try {
+      userModel.fullName = fullNameController.text;
+      userModel.email = emailController.text;
+      userModel.password = passwordController.text;
+      userModel.confirmPassword = confirmPasswordController.text;
+      await AuthService.to.createUserWithEmailAndPassword(
+          email: userModel.email!, password: userModel.password!);
+      Get.snackbar(
+          AppStrings.success, AppStrings.yourAccountWasSuccessfullyCreated);
+      log(AppStrings.yourAccountWasSuccessfullyCreated);
+    } on FirebaseAuthException catch (e) {
+      _handleFirebaseAuthException(e);
+    } catch (e) {
+      Get.snackbar(AppStrings.authenticationError, e.toString());
+      log(e.toString());
+    }
+  }
+
+  void _handleFirebaseAuthException(FirebaseAuthException e) {
+    String errorMessage;
+    switch (e.code) {
+      case AppStrings.userNotFound:
+        errorMessage = AppStrings.noUserFoundForThatEmail;
+        break;
+      case AppStrings.wrongPassword:
+        errorMessage = AppStrings.wrongPasswordProvidedForThatUser;
+        break;
+      case AppStrings.invalidEmail:
+        errorMessage = AppStrings.theEmailAddressIsBadlyFormatted;
+        break;
+      case AppStrings.userDisabled:
+        errorMessage =
+            AppStrings.theUserAccountHasBeenDisabledByAnAdministrator;
+        break;
+      case AppStrings.tooManyRequests:
+        errorMessage = AppStrings.tooManyRequestsTryAgainLater;
+        break;
+      case AppStrings.operationNotAllowed:
+        errorMessage = AppStrings.emailPasswordAccountsAreNotEnabled;
+        break;
+      case AppStrings.weakPassword:
+        errorMessage = AppStrings.thePasswordIsTooWeak;
+        break;
+      case AppStrings.emailAlreadyInUse:
+        errorMessage = AppStrings.theAccountAlreadyExistsForThatEmail;
+        break;
+      case AppStrings.networkRequestFailed:
+        errorMessage =
+            AppStrings.networkRequestFailedPleaseCheckYourInternetConnection;
+        break;
+      default:
+        errorMessage = AppStrings.anUndefinedError;
+    }
+    Get.snackbar(AppStrings.authenticationError, errorMessage);
   }
 }
