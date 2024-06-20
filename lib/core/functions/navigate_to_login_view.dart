83033b237c7d139c6d678392b801ecb605cb1770
mustafa sheha <dev.mustafa.sheha@gmail.com>
2024-06-20 23:03:02 +0300

    Add AuthBinding for AuthService, RegisterController, and LoginController.

--- lib/core/functions/navigate_to_login_view.dart
+++ lib/core/functions/navigate_to_login_view.dart
@@ -1,7 +1,8 @@
 import 'package:get/get.dart';
+import 'package:speedcircles/features/auth/bindings/auth_binding.dart';
 import 'package:speedcircles/features/auth/login/view/login_view.dart';
 
 
 void navigateToLoginView() {
-  Get.off(() => const LoginView());
+  Get.off(() => const LoginView(),binding: AuthBinding());
 }
